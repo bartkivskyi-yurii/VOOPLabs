e875961c48303459369a5e4ee1c71ebde33147b2
bartkivskyi-yurii <bartkivskyi.yu@knu.ua>
2026-05-21 16:30:51 +0300

    Added class constructors
    For Assignment and Discipline

--- Student_Progress_Tracker/Assignment.cs
+++ Student_Progress_Tracker/Assignment.cs
@@ -9,5 +9,14 @@ namespace Student_Progress_Tracker
         public string Title { get; set; }
         public string Type { get; set; }
         public double MaxPoints { get; set; }
+        public DateTime Deadline { get; set; }
+
+        public Assignment(string title, string type, double maxPoints, DateTime deadline)
+        {
+            Title = title;
+            Type = type;
+            MaxPoints = maxPoints;
+            Deadline = deadline;
+        }
     }
 }
--- Student_Progress_Tracker/Discipline.cs
+++ Student_Progress_Tracker/Discipline.cs
@@ -6,9 +6,22 @@ namespace Student_Progress_Tracker
 {
     internal class Discipline
     {
-        public string title;
-        public int totalLectures;
-        public int totalLabs;
-        public double maxPoints;
+        public string Title { get; set; }
+        public int TotalLectures { get; set; }
+        public int TotalLabs { get; set; }
+        public double MaxPoints { get; set; }
+
+        public Discipline(string title)
+        {
+            Title = title;
+        }
+
+        public Discipline(string title, int totalLectures, int totalLabs, double maxPoints)
+        {
+            Title = title;
+            TotalLectures = totalLectures;
+            TotalLabs = totalLabs;
+            MaxPoints = maxPoints;
+        }
     }
 }
